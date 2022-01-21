dc91d05654040079e7e3b2d14dfac031cdfc40a1
Francisco Hugo de Azevedo Aguiar <hugo-phone@live.com>
2022-01-21 18:27:18 -0300

    Servidor configurado...

---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import dotenv from 'dotenv';
+import mustache from 'mustache-express';
+import path from 'path';
+
+dotenv.config()
+
+const server = express()
+
+server.set('view engine', 'mustache')
+server.set('views', path.join(__dirname, 'views'))
+server.engine('mustache', mustache())
+
+server.use(express.static(path.join(__dirname, '../public')))
+
+server.use('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+server.listen(process.env.PORT)
